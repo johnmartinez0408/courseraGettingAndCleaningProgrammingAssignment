f84986b9d364c8da6c1c36e48f3bea2e25d01543
John Martinez <johnmartinez0408@gmail.com>
2016-10-14 15:38:58 -0400

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+#Read data from test
+testX = read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+testY = read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+testSubject = read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+#Combine data set with Activity number code and subject number
+testX$Activity = testY[,1]
+testX$Subject = testSubject[,1]
+
+
+#Read data from train
+trainX = read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+trainY = read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+trainSubject = read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+#Combine data set with Activity number code and subject number
+trainX$Activity = trainY[,1]
+trainX$Subject = trainSubject[,1]
+
+#Combine test and train data sets into one
+full = merge(testX,trainX,all=TRUE)
+
+
+full$Activity = as.character(full$Activity)
+
+#Replace each Activity code with the activity
+full$Activity = sub("1", "walking", full$Activity)
+full$Activity = sub("2", "walking_upstairs", full$Activity)
+full$Activity = sub("3", "waling_downstairs", full$Activity)
+full$Activity = sub("4", "sitting", full$Activity)
+full$Activity = sub("5", "standing", full$Activity)
+full$Activity = sub("6", "laying", full$Activity)
+
+
+#Read the column names from the features.txt file
+colNamesX = read.table("UCI HAR Dataset/features.txt", header = FALSE)
+names(full) = c(as.character(colNamesX[,2]),"Activity", "Subject")
+
+#Use grep and regex to get the indeces of the columns with names that are means or standard deviations
+meanStdIndeces = grep("mean\\(|std\\(", colNamesX[,2], ignore.case = TRUE)
+
+#Extract the specified rows from the full data set
+meanStdX = full[,meanStdIndeces]
+
+averages = meanStdX
+
+#Get the average for each variable
+for (i in ncol(averages)) {
+   averages[1,i] = mean(averages[,i]) 
+}
+
+averages = averages[1,]
+
+
+
+
